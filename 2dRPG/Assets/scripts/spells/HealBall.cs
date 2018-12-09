fffc7d48257b3ad05bffef7c1623f0311ca82205
robertosleandro <roberto.s.leandro@gmail.com>
2018-12-09 16:00:28 -0500

    -Added Spells in menu -Added fireball rotation -Adjusted size of spell icons -Added numbers to spell bar

---
+++ 2dRPG/Assets/scripts/spells/HealBall.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealBall : MonoBehaviour, Spell
+{
+    public void execute(Dictionary<string, object> context)
+    {
+        Player player = (Player) context["player"];
+
+        player.health += 50;
+        if (player.health > 500) player.health = 500;
+        player.healthbar.updateHealth(player.health, player.maxHealth);
+
+        Rigidbody2D casterRbody = (Rigidbody2D)context["casterRbody"];
+        Vector2 myPos = new Vector2(casterRbody.position.x, casterRbody.position.y);
+
+        Instantiate(gameObject, myPos, Quaternion.identity);
+    }
+
+    // Use this for initialization
+    void Start () {
+        //TODO need to implement a better way of destroying object after animation
+        Destroy(gameObject, 0.7f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ 2dRPG/Assets/scripts/spells/Spellbook.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spellbook{
+    public List<Spell> spells = new List<Spell>();
+
+    static Dictionary<string, Spell> initialSpells()
+    {
+        Dictionary<string, Spell> initialSpells = new Dictionary<string, Spell>();
+
+        fireball fireInstance = Resources.Load("fireball", typeof(fireball)) as fireball;
+        gasball gasInstance = Resources.Load("aoe", typeof(gasball)) as gasball;
+        HealBall healball = Resources.Load("healthball", typeof(HealBall)) as HealBall;
+
+        initialSpells.Add("fireball", fireInstance);
+        initialSpells.Add("gasball", gasInstance);
+        initialSpells.Add("healball", healball);
+
+        return initialSpells;
+    }
+
+    public Spellbook(List<string> inputedSpells)
+    {
+        for (int i = 0; i< inputedSpells.Count; i++)
+        {
+ //           Spell iSpell = Spellbook.initialSpells()[inputedSpells[i]];
+         //   iSpell.setTargetAndCaster(target, caster);
+
+            spells.Add(Spellbook.initialSpells()[inputedSpells[i]]);
+            Debug.Log("Spell " + inputedSpells[i] + ": " + Spellbook.initialSpells()[inputedSpells[i]]);
+        }
+    }
+
+
+}
